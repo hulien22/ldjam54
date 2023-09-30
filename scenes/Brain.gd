0520f59092dc5df7932e001704ca53052bf4861d
joseph dooley <josephldooley@gmail.com>
2023-09-30 04:14:07 -0400

    brain grid

---
+++ scenes/Brain.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+
+@export var Tile: PackedScene
+
+
+var memories=[]
+
+
+
+func _ready():
+	var coreMem = Tile.instantiate()
+	coreMem.init("tactic-1",Globals.L,Vector2(0,0),0)
+	coreMem.set_global_position(Vector2(250,100))
+	add_child(coreMem)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
