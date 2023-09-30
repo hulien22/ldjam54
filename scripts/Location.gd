62349b09bd4da16cfd93166c548f888f425e8c06
Eimh <41556634+Eimh@users.noreply.github.com>
2023-09-30 03:54:08 -0400

    init map stuff

---
+++ scripts/Location.gd
@@ -0,0 +1,35 @@
+class_name Location
+extends Control
+
+enum LOCATION {START, COMBAT, RECRUIT, LIBRARY, TRAINING, THERAPY, BOSS, RELAX}
+
+signal moved_to_location(Location)
+
+var location_type: LOCATION
+var connections: Array[Location]
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func activate(active: bool):
+	$button.disabled = !active
+
+func init(location_type: LOCATION, position: Vector2, active: bool, connections: Array[Location]):
+	self.location_type = location_type
+	self.connections = connections
+	self.set_position(position)
+	$button.disabled = !active
+	for connection in connections:
+		var line = Line2D.new()
+		line.add_point(Vector2.ZERO)
+		line.add_point(connection.position-position)
+		add_child(line)
+
+func _on_pressed():
+	print("here")
+	activate(false)
+	moved_to_location.emit(self)
+	for connection in connections:
+		connection.activate(true)
+	
---
+++ scripts/Map.gd
@@ -0,0 +1,78 @@
+extends Node
+
+@export var length: int
+@export var width: int
+
+@export var left_margin: int
+@export var right_margin: int
+@export var top_margin: int
+@export var bottom_margin: int
+
+@export var length_bias: float
+
+var current_location: Location
+var rng = RandomNumberGenerator.new()
+
+const location_scene = preload("res://scenes/location.tscn")
+
+func random_location() -> Location.LOCATION:
+	var value = rng.randi_range(1, 100)
+	if value <= 30:
+		return Location.LOCATION.COMBAT
+	elif value <= 40:
+		return Location.LOCATION.LIBRARY
+	elif value <= 50:
+		return Location.LOCATION.RECRUIT
+	elif value <= 70:
+		return Location.LOCATION.RELAX
+	elif value <= 80:
+		return Location.LOCATION.THERAPY
+	else:
+		return Location.LOCATION.TRAINING
+
+func _ready():
+	var total_length = get_viewport().get_visible_rect().size.x - right_margin - left_margin
+	var length_offset = total_length + left_margin
+	
+	var total_width = get_viewport().get_visible_rect().size.y - top_margin - bottom_margin
+	var center_height = total_width/2 + top_margin
+	
+	var boss = location_scene.instantiate()
+	var empty: Array[Location]
+	boss.init(Location.LOCATION.BOSS, Vector2(length_offset, center_height), false, empty)
+	add_child(boss)
+	boss.moved_to_location.connect(_on_moved_to_location)
+	
+	var prev_layer: Array[Location] = [boss]
+	for i in length:
+		var cur_layer: Array[Location] = []
+		length_offset -= total_length/(length+1)
+		var rand_width = rng.randi_range(1+floor(width*length_bias*(length-i)/length), width-floor(width*length_bias*i/length))
+		var connect_offset = len(prev_layer)/float(rand_width)
+		var width_ratio = ceil(connect_offset)
+		var width_offset = center_height if rand_width == 1 else top_margin
+		var is_active = i == length -1
+		for j in rand_width:
+			var connections: Array[Location] = []
+			for k in width_ratio:
+				connections.append(prev_layer[floor(j*connect_offset)+k])
+			var node = location_scene.instantiate()
+			node.init(random_location(),Vector2(length_offset, width_offset), is_active, connections)
+			add_child(node)
+			node.moved_to_location.connect(_on_moved_to_location)
+			width_offset += total_width/(rand_width-1)
+			cur_layer.append(node)
+		prev_layer = cur_layer
+			
+	var start = location_scene.instantiate()
+	start.init(Location.LOCATION.START, Vector2(left_margin, center_height), false, prev_layer)
+	add_child(start)
+	start.moved_to_location.connect(_on_moved_to_location)
+	
+	current_location = start
+
+func _on_moved_to_location(location: Location):
+	for connection in current_location.connections:
+		connection.activate(false)
+	current_location = location
+	print(location.position)
