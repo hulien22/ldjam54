d6b21997fc66766ad4dde956ff09fa4987c5e194
Eimh <41556634+Eimh@users.noreply.github.com>
2023-10-02 03:29:25 -0400

    fix repeated questions

--- scripts/Global.gd
+++ scripts/Global.gd
@@ -40,7 +40,7 @@ func load_debate_questions():
 			debate_questions[s].append(DebateQuestion.new(d[0], d[1], d[2], d[3], d[4], d[5]));
 
 func get_random_debate_question(difficulty) -> DebateQuestion:
-	return debate_questions[difficulty][rng.randi() % debate_questions.size()];
+	return debate_questions[difficulty][rng.randi() % debate_questions[difficulty].size()];
 
 enum TileState {
 	DRAGGABLE,
