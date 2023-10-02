93c788c2bc2d696692c6e602b5f07108fb41066d
Eimh <41556634+Eimh@users.noreply.github.com>
2023-10-02 12:37:50 -0400

    fix dumb godot bug

--- scripts/GameOver.gd
+++ scripts/GameOver.gd
@@ -1,6 +1,4 @@
 extends Node2D
 
-@export var menu_scene: PackedScene
-
 func _on_pressed():
-	get_tree().change_scene_to_packed(menu_scene)
+	get_tree().change_scene_to_file("res://scenes/mainmenu.tscn")
